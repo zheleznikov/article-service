512dd1d87835dc99b2ab51b8495567aeb9f71887
Сергей <polwen@yandex.ru>
2024-03-11 00:30:49 +0300

    add SecurityConfig

---
+++ src/main/java/com/zheleznikov/articleservice/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.zheleznikov.articleservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+
+        UserDetails user = User
+                .withUsername("user")
+                .password(passwordEncoder().encode("pwd"))
+                .roles("user")
+                .build();
+
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("pwd"))
+                .roles("admin", "user")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests ->
+                        requests.requestMatchers("/inner-api/**").hasRole("admin")
+                                .requestMatchers("/api/**").hasRole("user")
+                                .anyRequest().authenticated())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
+
